eef293ee42f4dc73d5c6f18124985932f5432411
Tuukka Norri <tsnorri@iki.fi>
2018-10-02 15:24:14 +0300

    Add boost_serialization to libraries

--- linux-static.local.mk
+++ linux-static.local.mk
@@ -11,5 +11,5 @@ CPPFLAGS				= -DNDEBUG
 
 BOOST_ROOT				= /home/tnorri/local/boost-1-67-0-clang++-5.0.2-libstdc++
 BOOST_INCLUDE			= -I$(BOOST_ROOT)/include
-BOOST_LIBS				= -L$(BOOST_ROOT)/lib -lboost_iostreams -lboost_system
+BOOST_LIBS				= -L$(BOOST_ROOT)/lib -lboost_iostreams -lboost_serialization -lboost_system
 LIBDISPATCH_LIBS		= ../lib/swift-corelibs-libdispatch/build/src/libdispatch.a /usr/lib/x86_64-linux-gnu/libkqueue.a /usr/lib/x86_64-linux-gnu/libBlocksRuntime.a -lbsd -lpthread -static-libstdc++ -static-libgcc
